63ae1f44bc5ca0832c314d5de793f3f3fa1188fb
DevByWater <egaraudy@gmail.com>
2017-05-01 16:06:45 -0600

    handling user auth input

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,19 +1,35 @@
 import React, {Component} from 'react'
 import {TextInput} from 'react-native'
-import { Button, Card, CardSection } from './common'
+import { Button, Card, CardSection, Input } from './common'
 
 class LoginForm extends Component {
-    state = { text: ''}
+    state = { email: '', password: ''}
 
     render(){
         return(
             <Card>
                 <CardSection>
-                    <TextInput underlineColorAndroid={'transparent'}  style={{height: 40, flex: 1}}/>
+                    <Input
+                        placeholder = "user@example.com"  
+                        label="Email"
+                        value = {this.state.email}
+                        onChangeText={email => this.setState({email})}
+                    />
                 </CardSection>
 
                 <CardSection>
-                    <Button>
+                    <Input
+                        secureTextEntry
+                        placeholder = "password"  
+                        label="Password"
+                        value = {this.state.password}
+                        onChangeText={password => this.setState({password})}
+                    />
+                </CardSection>
+
+
+                <CardSection>
+                    <Button onPress={this.onButtonPress.bind(this)}>
                     Log In
                     </Button>
                 </CardSection>
