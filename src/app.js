91a74c97f7e35dddbbe4d35092e827d68fd558bd
DevByWater <egaraudy@gmail.com>
2017-05-02 15:34:45 -0600

    Wrap up auth

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react'
 import { View, Text } from 'react-native'
 import firebase from 'firebase'
-import { Header } from './components/common'
+import { Header, Button, Spinner, Card, CardSection } from './components/common'
 import LoginForm from './components/LoginForm'
 
 
 class App extends Component {
+    state = { loggedIn: false }
     componentWillMount() {
         firebase.initializeApp({
             apiKey: "AIzaSyCypU0kzqxoK7oyA3Plp4L6VBS6rGodgds",
@@ -15,12 +16,39 @@ class App extends Component {
             storageBucket: "nativeauth-ce278.appspot.com",
             messagingSenderId: "981614179563"
         })
+
+        firebase.auth().onAuthStateChanged((user)=>{
+            if (user){
+                this.setState({loggedIn: true})
+            } else {
+                this.setState({ loggedIn: false})
+            }
+        })
     }
+
+    renderContent(){
+
+        switch(this.state.loggedIn){
+            case true:
+                return (
+                    <Card>
+                        <CardSection>
+                             <Button onPress={()=>firebase.auth().signOut()}>Log Out</Button>
+                        </CardSection>
+                    </Card> 
+                )
+            case false:
+                return <LoginForm />
+            default: 
+                return <Spinner />        
+        }
+    }
+
     render(){
         return(
             <View>
                 <Header headerText = 'Authentication' />
-                <LoginForm />
+                {this.renderContent()}
             </View>
         )
     }
